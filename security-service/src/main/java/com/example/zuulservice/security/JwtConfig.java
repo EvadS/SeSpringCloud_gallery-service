864d5e3cc97585378ea3a1fb0ca57b8ddf272327
Evgeniy Skiba <eskiba@s-pro.io>
2019-12-25 18:52:34 +0200

    fixed auth on secure-service

--- security-service/src/main/java/com/example/zuulservice/security/JwtConfig.java
+++ security-service/src/main/java/com/example/zuulservice/security/JwtConfig.java
@@ -1,6 +1,5 @@
 package com.example.zuulservice.security;
 
-
 import lombok.Getter;
 import lombok.ToString;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,39 +16,39 @@ import org.springframework.beans.factory.annotation.Value;
 @ToString
 public class JwtConfig {
 
-    @Value("${security.jwt.uri:/auth/**}")
-    private String Uri;
+   @Value("${security.jwt.uri:/auth/**}")
+   private String Uri;
 
-    @Value("${security.jwt.header:Authorization}")
-    private String header;
+   @Value("${security.jwt.header:Authorization}")
+   private String header;
 
-    @Value("${security.jwt.prefix:Bearer }")
-    private String prefix;
+   @Value("${security.jwt.prefix:Bearer }")
+   private String prefix;
 
-    @Value("${security.jwt.expiration:#{24*60*60}}")
-    private int expiration;
+   @Value("${security.jwt.expiration:#{24*60*60}}")
+   private int expiration;
 
-    @Value("${security.jwt.secret:JwtSecretKey}")
-    private String secret;
+   @Value("${security.jwt.secret:JwtSecretKey}")
+   private String secret;
 
-    public String getUri() {
-        return Uri;
-    }
+   public String getUri() {
+       return Uri;
+   }
 
-    public String getHeader() {
-        return header;
-    }
+   public String getHeader() {
+       return header;
+   }
 
-    public String getPrefix() {
-        return prefix;
-    }
+   public String getPrefix() {
+       return prefix;
+   }
 
-    public int getExpiration() {
-        return expiration;
-    }
+   public int getExpiration() {
+       return expiration;
+   }
 
-    public String getSecret() {
-        return secret;
-    }
+   public String getSecret() {
+       return secret;
+   }
 
 }
--- security-service/src/main/java/com/example/zuulservice/security/UserRole.java
+++ security-service/src/main/java/com/example/zuulservice/security/UserRole.java
@@ -1,5 +1,5 @@
 package com.example.zuulservice.security;
 
 public enum UserRole {
-    USER, ADMIN;
+	USER, ADMIN;
 }
